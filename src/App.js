6cba32719a160315ac7ca811d7521e829e82c771
Ramin Heshmati <r.heshmati@pixaldo.com>
2021-07-22 10:36:13 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Ramin
         </p>
         <a
           className="App-link"
